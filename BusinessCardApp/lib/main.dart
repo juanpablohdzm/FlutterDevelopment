3010d948cfe2eabf6ff2507d7ef3c243d0db878e
juanp <juanpablohdzm95@gmail.com>
2021-10-22 09:09:57 -0500

    safe area and widget containers.

--- BusinessCardApp/lib/main.dart
+++ BusinessCardApp/lib/main.dart
@@ -9,8 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.all(30.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
