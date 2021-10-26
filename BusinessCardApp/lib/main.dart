bb13ae160ed1da4c519127294832c60aa86adfb9
juanp <juanpablohdzm95@gmail.com>
2021-10-26 09:21:54 -0500

    Business card: Add profile picture and custom fonts.

--- BusinessCardApp/lib/main.dart
+++ BusinessCardApp/lib/main.dart
@@ -11,13 +11,28 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100,
-            width: 100,
-            margin: EdgeInsets.all(30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/picture.png'),
+              ),
+              Text(
+                'Angela Mck',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text('Developer',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal[100],
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico',
+                      letterSpacing: 2.5)),
+            ],
           ),
         ),
       ),
