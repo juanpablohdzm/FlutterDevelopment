3d06354db477f94836f4f3bef8eb07e8c1e50cce
juanp <juanpablohdzm95@gmail.com>
2021-11-11 10:56:43 -0600

    Finish dice example project.

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -27,6 +28,11 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 4;
   int rightDiceNumber = 1;
 
+  void changeDice() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   Widget build(BuildContext context) {
     return Center(
       child: Row(
@@ -38,7 +44,7 @@ class _DicePageState extends State<DicePage> {
                 child: Image.asset('images/dice$leftDiceNumber.png'),
                 onPressed: () {
                   setState(() {
-                    leftDiceNumber = 6;
+                    changeDice();
                   });
                 },
               ),
@@ -51,7 +57,7 @@ class _DicePageState extends State<DicePage> {
                 child: Image.asset('images/dice$rightDiceNumber.png'),
                 onPressed: () {
                   setState(() {
-                    rightDiceNumber = 5;
+                    changeDice();
                   });
                 },
               ),
